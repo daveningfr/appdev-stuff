ac01d407236bc091451f63e702045d96947a7d10
daveningfr <davenheng1234234@gmail.com>
2024-10-24 23:30:40 +0800

    q2

--- appdev/prac2/testapp.py
+++ appdev/prac2/testapp.py
@@ -4,6 +4,11 @@ import Student as s
 lecname = input("Enter lecturer name: ")
 lecnric = input("Enter lecturer nric: ")
 lecstaffid = input("Enter lecturer staff id: ")
+while lecnric != lecstaffid:
+    print("Staff ID must be the same as NRIC")
+    lecstaffid = input("Enter lecturer staff id: ")
+
+        
 lechours = int(input("Enter total teaching hours: "))
 lec = l.lecturer(lecname, lecnric, lecstaffid)
 lec.compute_salary(lechours)
@@ -13,7 +18,15 @@ snric = input("Enter student nric: ")
 sadminno = input("Enter student admin no: ")
 s = s.student(sname, snric, sadminno)
 stestmark = int(input("Enter student test mark: "))
+while stestmark <= 0 or stestmark >= 100:
+    print("Test mark must be between 0 and 100")
+    stestmark = int(input("Enter student test mark: "))
 sexammark = int(input("Enter student exam mark: "))
+
+while sexammark <= 0 or sexammark >= 100:
+    print("Exam mark must be between 0 and 100")
+    sexammark = int(input("Enter student exam mark: "))
+
 s.set_test_mark(stestmark)
 s.set_exam_mark(sexammark)
 s.compute_final_mark()
