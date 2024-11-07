e9e58d54a89feb1c651d8b6dc4a454f7b052fca0
daveningfr <davenheng1234234@gmail.com>
2024-11-07 14:57:16 +0800

    q2

--- appdev/exception/BMI.py
+++ appdev/exception/BMI.py
@@ -1,24 +1,34 @@
-while True:
-    name = input('Enter name: ')
-    weight = float(input('Enter weight (kg): '))
-    height = float(input('Enter height(m): '))
+try:
+    file = 'appdev/exception/BMI.txt'
+    while True:
+        name = input('Enter name: ')
+        weight = float(input('Enter weight (kg): '))
+        height = float(input('Enter height(m): '))
 
-    bmi = weight/height**2
-    print(bmi)
+        bmi = weight/height**2
+        print(bmi)
 
-    command = input('Store your bmi to file? (Y/N): ')
+        command = input('Store your bmi to file? (Y/N): ')
+        if command.upper() == 'Y':
+            bmi_File = open(file, 'a')
+            bmi_File.write(name+','+ str(bmi) +'\n')
+            bmi_File.close()
+        
+        command = input('Do you want to continue? (Y/N): ')
+        if command.upper() == 'N':
+            break
+
+    command = input('Do you want view BMI record in file? (Y/N): ')
     if command.upper() == 'Y':
-        bmi_File = open('bmi.txt', 'a')
-        bmi_File.write(name+','+ str(bmi) +'\n')
+        bmi_File = open(file, 'r')
+        contents = bmi_File.read()
+        print(contents)
         bmi_File.close()
-      
-    command = input('Do you want to continue? (Y/N): ')
-    if command.upper() == 'N':
-        break
-
-command = input('Do you want view BMI record in file? (Y/N): ')
-if command.upper() == 'Y':
-    bmi_File = open('bmi.txt', 'r')
-    contents = bmi_File.read()
-    print(contents)
-    bmi_File.close()
+except ValueError:
+    print('Invalid input')
+except ZeroDivisionError:
+    print('Cannot divide by zero')
+except IOError:
+    print('Error in file operation')
+except:
+    print('Unknown error')
